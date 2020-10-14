41afd00400d036dc1dea1d737b3e678fc95c9c0d
Christian Naths <christiannaths@gmail.com>
2020-10-14 14:02:32 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ErrorBoundary from "./ErrorBoundary";
+
+function SubComponent() {
+  const foo = {}.bar.baz;
+
+  return <div>foo</div>;
+}
 
 function App() {
   return (
@@ -10,14 +17,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ErrorBoundary>
+          <SubComponent />
+        </ErrorBoundary>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Bugsnag from "@bugsnag/js";
+import BugsnagPluginReact from "@bugsnag/plugin-react";
+import ErrorBoundary from "./ErrorBoundary";
+
+Bugsnag.start({
+  apiKey: "490338892dc4691e4b834d0f18d89266",
+  plugins: [new BugsnagPluginReact()],
+  releaseStage: process.env.REACT_APP_ENVIRONMENT,
+  // autoDetectErrors: false,
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ErrorBoundary>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ErrorBoundary>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
